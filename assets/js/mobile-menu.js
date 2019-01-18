8a535ae825d2803b5310268de6e6eb4998d1b15a
Crepiks <42639996+Crepiks@users.noreply.github.com>
2019-01-19 00:16:49 +0600

    Add html and css for header, make header responsive

---
+++ assets/js/mobile-menu.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.header__menu-open-button').on('click', openMobileMenu);
+    $('.top-nav__menu-close-button').on('click', closeMobileMenu);
+});
+
+function openMobileMenu(e) {
+    e.preventDefault();
+    $('.top-nav').addClass('top-nav_active');
+}
+
+function closeMobileMenu(e) {
+    e.preventDefault();
+    $('.top-nav').removeClass('top-nav_active');
+}
