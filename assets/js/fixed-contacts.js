66e09aae9f435f748e87bfe81c7338d19e1a0ad6
Crepiks <42639996+Crepiks@users.noreply.github.com>
2019-01-22 21:57:45 +0600

    Add fixed-contacts

---
+++ assets/js/fixed-contacts.js
@@ -0,0 +1,7 @@
+$('#fixed-contacts__phone-button').on('click', function() {
+    $('#fixed-contacts__animated-phone-text').toggleClass('fixed-contacts__animated-text_active');
+});
+
+$('#fixed-contacts__email-button').on('click', function() {
+    $('#fixed-contacts__animated-email-text').toggleClass('fixed-contacts__animated-text_active');
+})
