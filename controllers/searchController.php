d56e0a13107edf8e603a8adbef25ae6a5fd4206d
Crepiks <42639996+Crepiks@users.noreply.github.com>
2019-02-01 18:47:43 +0600

    Make person page

--- controllers/searchController.php
+++ controllers/searchController.php
@@ -25,6 +25,16 @@ if(isset($_COOKIE['lang']) && $_COOKIE['lang'] === 'kz') {
     $lang = 'ru';
 }
 
+if(isset($_GET['search'])){
+    $search = $_GET['search'];
+} else {
+    header('Location: /');
+}
+
+foreach ($articles as $key => $article) {
+    
+
+}
 
 
 // Добавляем новости в шаблон
@@ -36,3 +46,43 @@ $tpl->templ_parse();
 
 // Выводим шаблон
 echo $tpl->html;
+
+
+
+function contains($needle, $haystack) {
+    return strpos($haystack, $needle) !== false;
+}
+
+function get_ru_content($article) {
+    $content = '';
+
+     $content = '<div class="card mb-3 all-news-card">
+        <img src="/assets/img/news/' . $article['image'] . '" class="card-img-top" alt="Фотография статьи">
+        <div class="card-body">
+            <h5 class="card-title">' . $article['title_ru'] . '</h5>
+            <i>Дата публикации: ' . $article['date'] . '</i>
+            <p class="card-text">' . substr($article['text_ru'], 0, 300) . '...' . '</p>
+            <a href="/controllers/articleController.php?id=' . $article['id'] . '" class="btn btn-primary all-news-card__button">Подробнее</a>
+        </div>
+    </div>';
+
+
+    return $content;
+}
+
+function get_kz_content() {
+    $content = '';
+
+    $content = '<div class="card mb-3 all-news-card">
+        <img src="/assets/img/news/' . $article['image'] . '" class="card-img-top" alt="Фотография статьи">
+        <div class="card-body">
+            <h5 class="card-title">' . $article['title_kz'] . '</h5>
+            <i>Дата публикации: ' . $article['date'] . '</i>
+            <p class="card-text">' . substr($article['text_kz'], 0, 300) . '...' . '</p>
+            <a href="/controllers/articleController.php?id=' . $article['id'] . '" class="btn btn-primary all-news-card__button">Толығырақ</a>
+        </div>
+    </div>';
+    
+
+    return $content;
+}
