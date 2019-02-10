3e9bc85b1259a502ee72da29cd2e55ca5d463bc6
Crepiks <42639996+Crepiks@users.noreply.github.com>
2019-02-10 10:25:27 +0600

    Fix bag with article add

--- admin/adminControllers/addArticleController.php
+++ admin/adminControllers/addArticleController.php
@@ -6,14 +6,20 @@ require_once "../../models/articles.php";
 
 if (!empty($_POST)) {
     $connection = db_connect();
-    $title_kz = $_POST['title_kz'];
-    $title_ru = $_POST['title_ru'];
-    $text_kz = $_POST['text_kz'];
-    $text_ru = $_POST['text_ru'];
-    $date = $_POST['date'];
-    $image = $_POST['image'];
+    $title_kz = trim($_POST['title_kz']);
+    $title_ru = trim($_POST['title_ru']);
+    $text_kz = parse_text(trim($_POST['text_kz']));
+    $text_ru = parse_text(trim($_POST['text_ru']));
+    $date = trim($_POST['date']);
+    $image = trim($_POST['image']);
 
     addArticle($connection, $date, $image, $title_kz, $title_ru, $text_kz, $text_ru);
+
+}
+
+function parse_text($str) {
+    for ($result = '', $i = 0, $a = explode("\r\n", $str), $s = sizeof($a); $i < $s && ($result .= (!strlen($a[$i])?$a[$i]:'<p>'.$a[$i].'</p>')."\n") !== FALSE; ++$i); 
+    return $result;
 }
 
 header('Location: /admin/adminController.php');
