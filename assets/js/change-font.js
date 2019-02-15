b6b266fe1c5f65e01a85d04419b03b2b9e3930e6
Crepiks <42639996+Crepiks@users.noreply.github.com>
2019-02-15 20:41:44 +0600

    Fix font-size changing function

--- assets/js/change-font.js
+++ assets/js/change-font.js
@@ -4,16 +4,41 @@ var downButton = document.getElementById('font_down');
 upButton.addEventListener('click', upFont);
 downButton.addEventListener('click', downFont);
 
+
 function upFont(e) {
     e.preventDefault();
-    let fontSize = $('body').css('font-size') || '12px';
-    let newSize = parseInt(fontSize) * 1.1;
-    $('body').css('font-size', newSize + 'px');
+    $('.logo__text').css('font-size', '19px').css('font-weight', '500');
+    $('.top-nav__link').css('font-size', '20px');
+    $('.links-block__text').css('font-size', '19px');
+    $('.links-block__collapse-text').css('font-size', '19px');
+    $('.card-title').css('font-size', '30px');
+    $('.person-info__text-muted').css('font-size', '19px');
+    $('.footer__heading').css('font-size', '30px');
+    $('.footer__text').css('font-size', '23px');
+    $('.footer__link').css('font-size', '23px');
+    $('.footer__copyright-text').css('font-size', '25px');
+    $('.contact-bar__lang-button').css('font-weight', '500');
+    $('.odometer-block__text').css('font-size', '25px');
+    $('.article__title').css('font-size', '50px');
+    
+    $('body').css('font-size', '25px');
 }
 
 function downFont(e) {
     e.preventDefault();
-    let fontSize = $('body').css('font-size') || '12px';
-    let newSize = parseInt(fontSize) / 1.1;
-    $('body').css('font-size', newSize + 'px');
+    $('.logo__text').css('font-size', '17px').css('font-weight', '300');
+    $('.contact-bar__lang-button').css('font-weight', '300');
+    $('.top-nav__link').css('font-size', '1rem');
+    $('.links-block__text').css('font-size', '15px');
+    $('.links-block__collapse-text').css('font-size', '15px');
+    $('.card-title').css('font-size', '1.25rem');
+    $('.person-info__text-muted').css('font-size', '1rem');
+    $('.footer__heading').css('font-size', '25px');
+    $('.footer__text').css('font-size', '15px');
+    $('.footer__link').css('font-size', '15px');
+    $('.footer__copyright-text').css('font-size', '1rem');
+    $('.odometer-block__text').css('font-size', '1rem');
+    $('.article__title').css('font-size', '35px');
+
+    $('body').css('font-size', 'inherit');
 }
